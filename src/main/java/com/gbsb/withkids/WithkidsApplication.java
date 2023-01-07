381f3078f8c69ca2dcc1e777430cc59cdc829961
HANJIHEON <97011318+newHANJIHEON@users.noreply.github.com>
2023-01-07 19:47:04 +0900

    init

---
+++ src/main/java/com/gbsb/withkids/WithkidsApplication.java
@@ -0,0 +1,13 @@
+package com.gbsb.withkids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithkidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithkidsApplication.class, args);
+    }
+
+}
